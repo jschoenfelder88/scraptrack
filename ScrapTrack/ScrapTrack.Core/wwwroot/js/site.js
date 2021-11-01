a1305058ae033e1107067117ca4f4ba58cae6b5f
Joe Schoenfelder <jaschoenfelder@gmail.com>
2021-11-01 18:01:24 -0500

    Added sortable list feature. Modified Volunteers table to allow sorting by first of last name, and added volunteer details page to last name data column.

--- ScrapTrack/ScrapTrack.Core/wwwroot/js/site.js
+++ ScrapTrack/ScrapTrack.Core/wwwroot/js/site.js
@@ -22,3 +22,22 @@ function search() {
         }
     }
 }
+
+// Sorting
+// Get Value of the Rows
+const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
+    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+)(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+// do the work of sorting. 
+// note, this was a bit wonky because of how we are storing data in table body tags. 
+// This is version 2.0 and should work now
+document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+    const table = th.closest('table');
+    const tbody = table.querySelector('tbody');
+    Array.from(tbody.querySelectorAll('tr'))
+        .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+        .forEach(tr => tbody.appendChild(tr));
+})));
