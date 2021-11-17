55b745ee31a12a4922bc28809e8dcc4bedb4d505
EM078885 <emonroy.dev@gmail.com>
2021-11-17 00:35:23 -0600

    Specified the controller to allow view the volunteer details from the dashboard. Added seeding code to seed dev user.

--- ScrapTrack/ScrapTrack.Data/DataAccess/AuthorizationDbContext.cs
+++ ScrapTrack/ScrapTrack.Data/DataAccess/AuthorizationDbContext.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ScrapTrack.Data.Models;
+using System;
 
 namespace ScrapTrack.Data.DataAccess
 {
@@ -9,6 +11,27 @@ namespace ScrapTrack.Data.DataAccess
         public AuthorizationDbContext(DbContextOptions<AuthorizationDbContext> options)
         : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            this.SeedData(builder);
+        }
+
+        private void SeedData(ModelBuilder builder)
+        {
+            ApplicationUser devUser = new ApplicationUser()
+            {
+                UserName = "dev",
+                NormalizedUserName = "dev".ToUpperInvariant().Normalize(),
+                FirstName = "Dev",
+                LastName = "Team"
+            };
+
+            PasswordHasher<ApplicationUser> passwordHasher = new PasswordHasher<ApplicationUser>();
+            devUser.PasswordHash = passwordHasher.HashPassword(devUser, "aldojo");
+
+            builder.Entity<ApplicationUser>().HasData(devUser);
         }
     }
 }
