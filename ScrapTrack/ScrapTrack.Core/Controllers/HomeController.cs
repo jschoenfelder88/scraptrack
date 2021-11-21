694ae410169163c2a0ac8a1d319318e07171b8dc
Joe Schoenfelder <jaschoenfelder@gmail.com>
2021-11-20 21:02:26 -0600

    Created Reports Functionality. - The application is smart enough to read the database connection string dynamically, it does not need to be changed for each install/user. See models\db.cs for more details on how that string is accessed. - Currently only exports the Volunteers table. This will be updated when transactions table is finished. - ProcCreate.sql must be ran prior to utilizing of the function. This creates the necessary stored TSQL programming in the database for the application to call. - Two dependencies must be downloaded. EPPlus and System.Data.Sqlclient. For more information on why Sqlclient must be downloaded via nuget, see here:
    https://stackoverflow.com/questions/29733221/the-type-or-namespace-name-sqlconnection-could-not-be-found/48930113
    
    EPPlus required a slight modification of appsettings to designate the project's usage as non-commercial.

--- ScrapTrack/ScrapTrack.Core/Controllers/HomeController.cs
+++ ScrapTrack/ScrapTrack.Core/Controllers/HomeController.cs
@@ -10,12 +10,21 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using OfficeOpenXml;
 
 namespace ScrapTrack.Core.Controllers
 {
-    [Authorize]
+    [Authorize]    
     public class HomeController : Controller
     {
+        //database object
+        db dbop = new db();
+
         private readonly ILogger<HomeController> _logger;
 
         private readonly AppDataDbContext _context;
@@ -27,15 +36,33 @@ namespace ScrapTrack.Core.Controllers
 
         public IActionResult Index()
         {
+            DataSet ds = dbop.Getrecord();
             var dashboardModel = new DashboardViewModel()
             {
                 TransactionList = _context.Transactions.ToList(),
                 ItemList = _context.Items.Include(a => a.Category).ToList(),
                 VolunteerList = _context.Volunteers.ToList(),
                 CategoryId = new SelectList(_context.Categories, "Id", "Description")
-        };
+            };
 
             return View(dashboardModel);
+        }
+
+        //method for button Export Reports
+        public IActionResult ExporttoExcel()
+        {
+            DataSet ds = dbop.Getrecord();
+            var stream = new MemoryStream();
+
+            using (var package = new ExcelPackage(stream))
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+                worksheet.Cells.LoadFromDataTable(ds.Tables[0], true);
+                package.Save();
+            }
+            stream.Position = 0;
+            string excelname = $"ScrapTrack-Report-{DateTime.Now.ToString("yyyyMMddHHmmssfff")}.xlsx";
+            return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", excelname);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
